7b2667d0088dfab8bf12d447b02fad067e4606a6
Geoff McLane <ubuntu@geoffair.info>
2017-03-30 02:52:41 +0200

    Issue #2: Code only for WIN32

--- src/DumpLib.c
+++ src/DumpLib.c
@@ -3,9 +3,10 @@
 // Dump of COFF ARCHIVE (*.lib) file
 #include	"Dump4.h"
 #include <time.h>
-#include <Winsock2.h>
 
-#ifndef USE_PEDUMP_CODE // FIX20080507
+// #ifndef USE_PEDUMP_CODE // FIX20080507
+#if !defined(USE_PEDUMP_CODE) && defined(_WIN32) // FIX20080507
+#include <Winsock2.h>
 
 extern void ProcessHex( PBYTE pb, DWORD len );
 
